9c150000822c06318e746d65200759cd0bf3d11d
zhangyijie110 <1109721716@.com>
2019-10-25 22:35:19 +0800

    登录页面绘制完成

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    hm-toutiap-pc
-    <el-button type="success">成功按钮</el-button>
+    <!-- 一级路由 -->
+    <router-view />
   </div>
 </template>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 import VueRouter from 'vue-router'
 import Vue from 'vue'
 
+import Login from '@/views/login'
+
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   // 路由配置对象
   // 关键选项：routes 作用：配置路由规则
-  routes: []
+  routes: [{
+    // 登录
+    path: '/login',
+    component: Login
+  }]
 })
 
 export default router
