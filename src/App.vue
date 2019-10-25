334a3fe0c5d9ba65febccee60a2e1cf25ae1a5b7
zhangyijie110 <1109721716@.com>
2019-10-25 21:32:31 +0800

    使用饿了么UI库

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
-  <div id="app">hm-toutiap-pc</div>
+  <div id="app">
+    hm-toutiap-pc
+    <el-button type="success">成功按钮</el-button>
+  </div>
 </template>
 
 <script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+
+// ElementUI 是一个插件  集成了很多组件
+Vue.use(ElementUI)
+
 Vue.config.productionTip = false
 
 new Vue({
